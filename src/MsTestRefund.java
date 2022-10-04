aeeed9d763094042a3df9a2361f74d3edb777d74
서정희 <sjhlko@naver.com>
2022-10-04 14:48:09 +0900

    print문 ' , '뒤 띄어쓰기로 수정

--- src/MsTestRefund.java
+++ src/MsTestRefund.java
@@ -11,17 +11,17 @@ public class MsTestRefund {
         //5000원권은 1장만 나오게 하고 싶다. 왜냐하면 앞에서 만원권으로 처리했기때문
         //refund-=20000;
         refund%=10000;
-        System.out.printf("5천원권 %d장 나머지:%d\n",refund/curr5000, refund%curr5000);
+        System.out.printf("5천원권 %d장 나머지:%d\n", refund/curr5000, refund%curr5000);
         refund%=5000;
-        System.out.printf("천원권 %d장 나머지:%d\n",refund/curr1000, refund%curr1000);
+        System.out.printf("천원권 %d장 나머지:%d\n", refund/curr1000, refund%curr1000);
         refund%=1000;
-        System.out.printf("오백원권 %d개 나머지:%d\n",refund/500, refund%500);
+        System.out.printf("오백원권 %d개 나머지:%d\n", refund/500, refund%500);
         refund%=500;
-        System.out.printf("백원권 %d개 나머지:%d\n",refund/100, refund%100);
+        System.out.printf("백원권 %d개 나머지:%d\n", refund/100, refund%100);
         refund%=100;
-        System.out.printf("오십원권 %d개 나머지:%d\n",refund/50, refund%50);
+        System.out.printf("오십원권 %d개 나머지:%d\n", refund/50, refund%50);
         refund%=50;
-        System.out.printf("십원권 %d개 나머지:%d\n",refund/10, refund%10);
+        System.out.printf("십원권 %d개 나머지:%d\n", refund/10, refund%10);
         refund%=10;
         //같은 단어 여러개 선택 : art+j;
     }
