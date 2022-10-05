34a124ff95bbb3e55c8ac9158f31f7b0cc6bd052
서정희 <sjhlko@naver.com>
2022-10-05 10:22:59 +0900

    calculator 실습 (main과 클래스 따로 작성)

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+    public void plus(int a, int b){
+        System.out.println(a+b);
+    }
+    public void minus(int a, int b){
+        System.out.println(a-b);
+    }
+    public void multiple(int a, int b){
+        System.out.println(a*b);
+    }
+    public void devide(int a, int b){
+        System.out.println(a/b);
+    }
+
+}
---
+++ src/CalculatorMain.java
@@ -0,0 +1,9 @@
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.plus(1,2);
+        calculator.minus(1,2);
+        calculator.devide(1,2);
+        calculator.multiple(1,2);
+    }
+}
