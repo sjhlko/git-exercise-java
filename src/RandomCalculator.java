508122e57c9b8f32b81b6d399ebd0fd1cf079032
서정희 <sjhlko@naver.com>
2022-10-05 11:48:45 +0900

    랜덤 계산기 수정

--- src/RandomCalculator.java
+++ src/RandomCalculator.java
@@ -5,26 +5,35 @@ public class RandomCalculator {
 
     public RandomCalculator(int a) {
         this.a=a;
+
+    }
+    public void getRandom(){
         Random random = new Random();
         this.ranNum = random.nextInt()%10;
+        System.out.println("랜덤한 숫자 "+ this.ranNum);
     }
 
     public void plus(){
+        getRandom();
         System.out.println("plus"+(a+ranNum));
     }
     public void minus(){
+        getRandom();
         System.out.println("minus "+(a-ranNum));
     }
     public void multiple(){
+        getRandom();
         System.out.println("multiple "+(a*ranNum));
     }
     public void devide(){
+        getRandom();
         if(ranNum==0){
             System.out.println("랜덤하게 선택된 숫자가 0이므로 나누기 연산 불가능");
             return;
         }
+
         float result = (float)a/(float)ranNum;
-        System.out.println("devide "+(a*ranNum));
+        System.out.println("devide "+result);
     }
 
 }
