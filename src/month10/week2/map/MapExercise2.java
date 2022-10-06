6c70befbe2bc6ff726d0863c1bfeea95a1ba8094
서정희 <sjhlko@naver.com>
2022-10-06 13:41:09 +0900

    대문자로 수정

--- src/month10/week2/map/MapExercise2.java
+++ src/month10/week2/map/MapExercise2.java
@@ -3,18 +3,26 @@ package month10.week2.map;
 import java.util.HashMap;
 
 public class MapExercise2 {
-    public boolean isAlphabet(char alphabet){
-        if((alphabet>='A'&&alphabet<'Z')||(alphabet>'a'&&alphabet<'z')){
+    public boolean isAlphabet(char alphabet) {
+        if ((alphabet >= 'A' && alphabet < 'Z') || (alphabet > 'a' && alphabet < 'z')) {
             return true;
 
-        }
-        else return false;
+        } else return false;
     }
+
     public static void main(String[] args) {
+        MapExercise2 mapExercise2 = new MapExercise2();
         HashMap alphabet= new HashMap<Character,Integer>();
-        String s = new String("https://github.com/dongyeon-0822/java-project-exercise");
+        String s = new String("https://github.com/dongyeon-0822/java-project-exercise").toUpperCase();
+        /*
+        * 강사님 풀이) 미리 A~Z까지 넣어놓는다 0개로
+        * for(char c='A';c<='Z';c++){
+        *   alphabet.put(c,0);
+        * }
+        *
+        * */
         for(int i=0;i<s.length();i++){
-            if((s.charAt(i)>='A'&&s.charAt(i)<'Z')||(s.charAt(i)>'a'&&s.charAt(i)<'z')){
+            if((mapExercise2.isAlphabet(s.charAt(i)))){
                 int num=1;
                 if(alphabet.get(s.charAt(i))!=null){
                     num=(int)alphabet.get(s.charAt(i))+1;
