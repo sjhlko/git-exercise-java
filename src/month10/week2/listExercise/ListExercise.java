5ab28a35f0478eb5f2a989f94741bf89dc9c93d9
서정희 <sjhlko@naver.com>
2022-10-06 09:41:07 +0900

    List Exercise (4개 추가 예시)

---
+++ src/month10/week2/listExercise/ListExercise.java
@@ -0,0 +1,9 @@
+package month10.week2.listExercise;
+
+public class ListExercise {
+    this.students = new ArrayList<>();
+    this.students.add("김경록");
+    this.students.add("김미지");
+    this.students.add("김민경");
+    this.students.add("김민지");
+}
