7e46f7ea00a1962e74231a49c5ee06ec299d2283
서정희 <sjhlko@naver.com>
2022-10-06 10:25:08 +0900

    랜덤 숫자 생성기 중복 제거

--- src/month10/week2/set/RndNumbersWithoutDuplicated.java
+++ src/month10/week2/set/RndNumbersWithoutDuplicated.java
@@ -1,11 +1,18 @@
 package month10.week2.set;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RndNumbersWithoutDuplicated {
     public static void main(String[] args) {
         RandomNuberGenerator randomNuberGenerator = new RandomNuberGenerator();
+        HashSet<Integer> numbers = new HashSet<>();
         for(int i=0;i<50;i++){
-           int r= randomNuberGenerator.generate(10);
-            System.out.println(r);
+            int r= randomNuberGenerator.generate(10);
+            numbers.add(r);
+
         }
+        System.out.println(numbers);
+        System.out.println(numbers.size());
     }
 }
