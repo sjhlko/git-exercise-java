6ed55da265d853c25745835ee56caa8f2827da3f
서정희 <sjhlko@naver.com>
2022-10-06 11:49:23 +0900

    알파벳 개수 새기 알고리즘

---
+++ src/month10/week2/map/MapExercise2.java
@@ -0,0 +1,22 @@
+package month10.week2.map;
+
+import java.util.HashMap;
+
+public class MapExercise2 {
+    public static void main(String[] args) {
+        HashMap alphabet= new HashMap<Character,Integer>();
+        String s = new String("https://github.com/dongyeon-0822/java-project-exercise");
+        for(int i=0;i<s.length();i++){
+            if((s.charAt(i)>='A'&&s.charAt(i)<'Z')||(s.charAt(i)>'a'&&s.charAt(i)<'z')){
+                int num=1;
+                if(alphabet.get(s.charAt(i))!=null){
+                    num=(int)alphabet.get(s.charAt(i))+1;
+                }
+                alphabet.put(s.charAt(i),(Integer)num);
+
+            }
+        }
+        System.out.println(alphabet);
+
+    }
+}
