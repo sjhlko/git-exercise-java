7b224f53f35142e758edb269dc645928ae526e09
서정희 <sjhlko@naver.com>
2022-10-13 13:14:40 +0900

    address 파싱 추가

--- src/month10/week3/hospital/com/line/domain/Hospital.java
+++ src/month10/week3/hospital/com/line/domain/Hospital.java
@@ -2,11 +2,21 @@ package month10.week3.hospital.com.line.domain;
 
 public class Hospital {
     private  String id;
+    private  String address;
 
     public Hospital parse;
 
     public Hospital(String s) {
-        this.id=s;
+        this.id=s.replaceAll("\"","");
+    }
+
+    public Hospital(String id, String address) {
+        this.id = id;
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
     }
 
     public String getId() {
--- src/month10/week3/hospital/com/line/parser/HospitalParser.java
+++ src/month10/week3/hospital/com/line/parser/HospitalParser.java
@@ -4,9 +4,12 @@ import month10.week3.hospital.com.line.domain.Hospital;
 
 public class HospitalParser implements Parser<Hospital> {
 
+    private  String replaceAllQuot(String str){
+        return str.replaceAll("\"","");
+    }
     @Override
     public Hospital parse(String str) {
         String[] splitted = str.split(",");
-        return new Hospital(splitted[0]);
+        return new Hospital(replaceAllQuot(splitted[0]),replaceAllQuot(splitted[1]));
     }
 }
