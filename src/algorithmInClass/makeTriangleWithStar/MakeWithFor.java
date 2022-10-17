acb6ed32ebc1ba0e5668a6ee40e108e6820cf139
서정희 <sjhlko@naver.com>
2022-10-17 09:30:15 +0900

    별찍기(for문)

---
+++ src/algorithmInClass/makeTriangleWithStar/MakeWithFor.java
@@ -0,0 +1,16 @@
+package algorithmInClass.makeTriangleWithStar;
+
+import java.util.Scanner;
+
+public class MakeWithFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
