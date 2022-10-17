3dbefc282ca821c1d937d49f8d9bff1218a5bb14
서정희 <sjhlko@naver.com>
2022-10-17 09:39:20 +0900

    별찍기(for문) 다른 문자로도 수행되게 리팩토링(생성자 이용)

--- src/algorithmInClass/makeTriangleWithStar/MakeWithFor.java
+++ src/algorithmInClass/makeTriangleWithStar/MakeWithFor.java
@@ -3,12 +3,20 @@ package algorithmInClass.makeTriangleWithStar;
 import java.util.Scanner;
 
 public class MakeWithFor {
+    private  String letter = "*";
+    public MakeWithFor() {
+    }
+    public MakeWithFor(String letter) {
+        this.letter = letter;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        MakeWithFor makeWithFor = new MakeWithFor("@");
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < i+1; j++) {
-                System.out.print("* ");
+                System.out.print(makeWithFor.letter+" ");
             }
             System.out.println("");
         }
