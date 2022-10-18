c6acb84782d583b5a4050ca983e4911a7ed0096a
서정희 <sjhlko@naver.com>
2022-10-18 09:25:45 +0900

    직사각형 별찍기

---
+++ src/algorithmInClass/makeSquare/MakeRectangle.java
@@ -0,0 +1,17 @@
+package algorithmInClass.makeSquare;
+
+import java.util.Scanner;
+
+public class MakeRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
