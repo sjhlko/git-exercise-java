5cb590cac5a5ac5d50d1ab96c36710b43c0d14b9
서정희 <sjhlko@naver.com>
2022-10-21 11:02:03 +0900

    중괄호 대괄호 추가

--- src/algoStudy/week2/Programmers12909.java
+++ src/algoStudy/week2/Programmers12909.java
@@ -1,6 +1,7 @@
 package algoStudy.week2;
 
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Programmers12909 {
@@ -20,9 +21,60 @@ public class Programmers12909 {
         return stack.empty();
     }
     public boolean solution2(String s){
-        while(s.indexOf("()")!=-1){
-            s= s.replaceAll("()","");
+        while(s.contains("()")){
+            s= s.replaceAll("\\(\\)","");
         }
         return s.length() == 0;
     }
+
+    public boolean solution3(String s){
+        while(s.contains("()")){
+            String[] splitted = s.split("\\(\\)");
+            s = String.join("",splitted);
+
+        }
+        return s.length() == 0;
+    }
+
+    public boolean solutionForAllBrackets(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length();i++){
+            char nextpop = 0;
+            switch (s.charAt(i)) {
+                case '(':
+                case '{':
+                case '[':
+                    st.push(s.charAt(i));
+                    continue;
+                case ')':
+                    nextpop = '(';
+                    break;
+                case '}':
+                    nextpop = '{';
+                    break;
+                case ']':
+                    nextpop = '[';
+                    break;
+            }
+            if(st.empty())
+                return false;
+            if(st.peek()!=nextpop)
+                return false;
+            st.pop();
+        }
+        return st.empty();
+    }
+
+//    public static void main(String[] args) {
+//        Programmers12909 ps = new Programmers12909();
+//        Scanner sc = new Scanner(System.in);
+//        String s =sc.next();
+//        String[] splitted = s.split("\\.");
+//        for (int i = 0; i < splitted.length; i++) {
+//            System.out.println(ps.solutionForAllBrackets(splitted[i]));
+//
+//        }
+//
+//    }
+
 }
--- test/algoStudy/week2/Programmers12909Test.java
+++ test/algoStudy/week2/Programmers12909Test.java
@@ -15,9 +15,23 @@ class Programmers12909Test {
     }
 
     @Test
-    @DisplayName("강사님 풀이")
+    @DisplayName("강사님 풀이(괄호를 하나씩 삭제)")
     void solution2() {
         Programmers12909 sb = new Programmers12909();
         assertTrue(sb.solution2("()()"));
+    }
+
+    @Test
+    @DisplayName("강사님 풀이(splitted 이용)")
+    void solution3() {
+        Programmers12909 sb = new Programmers12909();
+        assertTrue(sb.solution3("()()"));
+    }
+
+    @Test
+    @DisplayName("중괄호 대괄호")
+    void solution4() {
+        Programmers12909 sb = new Programmers12909();
+        assertTrue(sb.solutionForAllBrackets("({}[])"));
     }
 }
