894d5e39dd2c76c46681e709931b1c96c1558a01
서정희 <sjhlko@naver.com>
2022-10-26 11:14:17 +0900

    Hash 충돌 해결 추가

--- src/algorithmInClass/hash/HashTable.java
+++ src/algorithmInClass/hash/HashTable.java
@@ -1,18 +1,37 @@
 package algorithmInClass.hash;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class HashTable {
+    class Node{
+        private String key;
+        private Integer value;
+
+        public Node(String key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+    }
     private int size = 10000;
-    private int[] table = new int[size];
+    List<Node>[] table = new ArrayList[size];
 
     public HashTable() {
     }
 
     public HashTable(int size) {
         this.size = size;
-        this.table = new int[size];
+        this.table = new ArrayList[size];
     }
 
     public int hash(String key) {
@@ -24,22 +43,30 @@ public class HashTable {
     }
 
     public void insert(String key, Integer value) {
-        this.table[hash(key)] = value;
+        int hashIdx = hash(key);
+        if(this.table[hashIdx]==null){
+            this.table[hashIdx]=new ArrayList<>();
+        }
+        this.table[hashIdx].add(new Node(key,value));
     }
 
-    public int search(String key) {
-        return this.table[hash(key)];
+    public Integer search(String key) {
+        List<Node> nodes = this.table[hash(key)];
+        for (Node node : nodes) {
+            if(key.equals(node.getKey())){
+                return node.value;
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-        String[] names = new String[]{"DongyeonKang",
-                "SubinKang", "KwanwunKo", "HyunseokKo", "KyoungdukKoo", "YeonjiGu", "SoyeonKown", "OhsukKwon", "GunwooKim", "KiheonKim", "NayeongKim", "DohyeonKim", "MinkyoungKim", "MinjiKim", "SanghoKim", "SolbaeKim", "YejinKim", "EungjunKim", "JaegeunKim", "JeonghyeonKim", "JunhoKim", "JisuKim", "kimjinah", "HaneulKim", "HeejungKim", "KimoonPark", "EunbinPark", "JeongHoonPark", "JeminPark", "TaegeunPark", "JiwonBae", "SeunggeunBaek", "JihwanByeon", "HeungseopByeon", "JeongHeeSeo", "TaegeonSeo", "SeeYunSeok", "SuyeonSeong", "SeyoelSon", "MinjiSong", "JinwooSong", "hyunboSim", "SominAhn", "JiyoungAhn", "ChangbumAn", "SoonminEom",
+        HashTable ht = new HashTable(1000);
+        ht.insert("hi",1);
+        ht.insert("ih",2);
 
-                "HyeongsangOh", "SuinWoo", "JuwanWoo", "InkyuYoon", "GahyunLee", "DaonLee", "DohyunLee", "SanghunLee", "SujinLee", "AjinLee", "YeonJae", "HyeonjuLee", "HakjunYim", "SeoyunJang", "SeohyeonJang", "JinseonJang", "SujinJeon", "SeunghwanJeon", "DaehwanJung", "JaeHyunJeung", "HeejunJeong", "GukhyeonCho", "MunjuJo", "YejiJo", "ChanminJu", "MinjunChoi", "SujeongChoi", "SeunghoChoi", "AyeongChoi", "GeonjooHan", "JinhyuckHeo", "MinwooHwang", "SieunHwang",
-                "JunhaHwang"};
-        HashTable ht = new HashTable(200);
-        for (int i = 0; i < names.length; i++) {
-            ht.insert(names[i], ht.hash(names[i]));
+        if(ht.search("ih")==2){
+            System.out.println("good");
         }
 
     }
