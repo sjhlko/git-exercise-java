7aebf459f930d4e2355761280c13e9c0e3c5fba8
서정희 <sjhlko@naver.com>
2022-10-27 10:11:06 +0900

    강사님 로직 추가(이름 중복 해결 안됨)

--- src/algorithmInClass/hash/Programmers42576.java
+++ src/algorithmInClass/hash/Programmers42576.java
@@ -1,11 +1,32 @@
 package algorithmInClass.hash;
 
 import java.util.HashMap;
+import java.util.Map;
 
 //완주하지 못한 선수 풀이
 //https://school.programmers.co.kr/learn/courses/30/lessons/42576
 public class Programmers42576 {
+    class SolutionByKKR {
 
+        //이렇게 될 시에 이름 중복이 해결이 안됨
+        public String solution1(String[] participant, String[] completion) {
+            Map<String, Integer> memo = new HashMap<>();
+            for (int i = 0; i < participant.length; i++) {
+                String key = participant[i];
+                memo.put(key, 1);
+            }
+            for (int i = 0; i < completion.length; i++) {
+                String key = completion[i]; // value 1
+                memo.put(key, 0);
+            }
+            for (String key : memo.keySet()) {
+                if (memo.get(key) == 1) {
+                    return key;
+                }
+            }
+            return null;
+        }
+    }
     class Solution {
         public String solution(String[] participant, String[] completion) {
             String answer="";
