b1ff52b77f69c808d1e169c10d290285d3096965
서정희 <sjhlko@naver.com>
2022-10-31 10:50:13 +0900

    프로그래머스 모의고사(완전탐색) 풀이성공

--- src/algorithmInClass/BruteForce/Programmers42840.java
+++ src/algorithmInClass/BruteForce/Programmers42840.java
@@ -1,23 +1,41 @@
 package algorithmInClass.BruteForce;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Programmers42840 {
-    public static int solution(int[] answers) {
-        int firstStudentAnswerCnt = 0;
-        int[] firstPattern = {1,2,4,4,5};
+    public int[] solution(int[] answers) {
+        int[] answer = new int[3];
+        int[] firstPattern = {1,2,3,4,5};
+        int[] secondPattern = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] thirdPattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         int pointer = 0;
         while (pointer<answers.length){
-            if(firstPattern[pointer%5]==answers[pointer]){
-                firstStudentAnswerCnt++;
-            }
+            if(firstPattern[pointer%5]==answers[pointer])
+                answer[0]++;
+            if(secondPattern[pointer%8]==answers[pointer])
+                answer[1]++;
+            if(thirdPattern[pointer%10]==answers[pointer])
+                answer[2]++;
             pointer++;
-
         }
-        System.out.println(firstStudentAnswerCnt);
-        return firstStudentAnswerCnt;
+        int max = -1;
+        for(int i=0;i<answer.length;i++){
+            if(max<=answer[i])
+                max=answer[i];
+        }
+        List<Integer> ansList = new ArrayList<>();
+        for(int i=0;i<answer.length;i++){
+            if(max==answer[i]){
+                ansList.add(i+1);
+            }
+        }
+        int[] ansPrint = new int[ansList.size()];
+        for (int i = 0 ; i < ansList.size() ; i++) {
+            ansPrint[i] = ansList.get(i);
+        }
+        return ansPrint;
     }
 
-    public static void main(String[] args) {
-        int[] a = {1,2,3,4,5,1,2};
-        System.out.println(solution(a));
-    }
 }
