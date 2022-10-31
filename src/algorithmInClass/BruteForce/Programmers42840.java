efa33fd9c3ab63f7f97046d225e150f695cd89a1
서정희 <sjhlko@naver.com>
2022-10-31 10:53:46 +0900

    프로그래머스 모의고사(완전탐색) 강사님 풀이 추가

--- src/algorithmInClass/BruteForce/Programmers42840.java
+++ src/algorithmInClass/BruteForce/Programmers42840.java
@@ -6,6 +6,22 @@ import java.util.List;
 
 public class Programmers42840 {
     public int[] solution(int[] answers) {
+
+        //강사님 풀이
+        //        String answerPattern1 = "12345".repeat(4000);
+        //        String answerPattern2 = "21232425".repeat(1250);
+        //        String answerPattern3 = "3311224455".repeat(1000);
+        //
+        //// 공간복잡도로 속도를 높이는 방법
+        //
+        //        int firstStudentAnswerCnt = 0;
+        //// 12345 패턴으로 찍는 학생이 몇 문제를 맞추었는지 알아내는 코드
+        //        for (int i = 0; i < answer.length; i++) {
+        //            if(Character.getNumericValue(answerPattern1.charAt(i)) == answer[i]) firstStudentAnswerCnt++;
+        //        }
+        //
+        //        System.out.println(firstStudentAnswerCnt);
+
         int[] answer = new int[3];
         int[] firstPattern = {1,2,3,4,5};
         int[] secondPattern = {2, 1, 2, 3, 2, 4, 2, 5};
