53d0250144e9b822d317a67a9801c850aff2c815
서정희 <sjhlko@naver.com>
2022-11-02 09:54:48 +0900

    에라토스테네스의 채 알고리즘 1단계- IndexOutOfBoundsException 해결

--- src/algorithmInClass/primeNumber/Eratostenes.java
+++ src/algorithmInClass/primeNumber/Eratostenes.java
@@ -7,10 +7,11 @@ public class Eratostenes {
     public static void main(String[] args) {
         int N = 50;
         List<Integer> pn = new ArrayList<>();
-        for(int i=1;i<=N;i++){
+        for(int i=2;i<=N;i++){
             pn.add(i);
         }
-        for (int i = 0; i <N; i++) {
+        //조건을 N까지로 할경우 pn의 사이즈가줄어들며 인덱스를 넘어가는 오류 발생
+        for (int i = 1; i < pn.size(); i++) {
             if(pn.get(i)%2==0)
                 pn.remove(i);
         }
