41d9ca80df9d2e64ef02351af23ccb7665dcfbfe
서정희 <sjhlko@naver.com>
2022-11-02 10:15:18 +0900

    에라토스테네스의 채 알고리즘 -리스트 이용

--- src/algorithmInClass/primeNumber/Eratostenes.java
+++ src/algorithmInClass/primeNumber/Eratostenes.java
@@ -10,11 +10,14 @@ public class Eratostenes {
         for(int i=2;i<=N;i++){
             pn.add(i);
         }
-        //조건을 N까지로 할경우 pn의 사이즈가줄어들며 인덱스를 넘어가는 오류 발생
-        for (int i = 1; i < pn.size(); i++) {
-            if(pn.get(i)%2==0)
-                pn.remove(i);
+        for(int j=2;j*j<=N;j++){
+            //조건을 N까지로 할경우 pn의 사이즈가줄어들며 인덱스를 넘어가는 오류 발생
+            for (int i = 1; i < pn.size(); i++) {
+                if(pn.get(i)%j==0&&pn.get(i)!=j)
+                    pn.remove(i);
+            }
         }
+
         System.out.println(pn);
     }
 }
