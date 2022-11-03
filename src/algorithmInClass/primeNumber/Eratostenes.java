266707e21e14ee794c8c717f87d359808de3f5c8
서정희 <sjhlko@naver.com>
2022-11-03 10:14:11 +0900

    에라토스테네스의 채 알고리즘- 2의 배수 지우기(배열 전체가 아니라 2의 배수인것에만 접근하여 효율성 높임)

--- src/algorithmInClass/primeNumber/Eratostenes.java
+++ src/algorithmInClass/primeNumber/Eratostenes.java
@@ -20,19 +20,49 @@ public class Eratostenes {
 //            }
 //        }
 
-        int[] pn = new int[51];
-        for (int i = 2; i <= N; i++) {
-            pn[i]=i;
+//        int[] pn = new int[N+1];
+//
+//
+//        for (int i = 2; i <= N; i++) {
+//            pn[i]=i;
+//        }
+
+        //자기 자신을 제외한 배수들을 지우기 위해 자기 자신이 아닌 배수들중에 가장 작은 배열 출력
+        //에라토스테네스 체의 속도 개선의 핵심은
+        // 1. list를 사용할때 add remove 하지 않기
+        // 2. 전체배열을 다 확인하여 배수를 지우는게 아니라 배수에만 접근하여 지울 수 있음
+
+
+        //이렇게 할 경우 배열을 매번 전부 접근하기때문에 비효율적
+//        for (int i = 2; i*i <= N; i++) {
+//            for(int j=i+1;j<=N;j++){
+//                if(pn[j]%i==0)
+//                    pn[j]=-1;
+//            }
+//        }
+//        for (int i = 2; i <= N ; i++) {
+//            if(pn[i]!=-1)
+//                System.out.println(pn[i]);
+//        }
+
+        //2~50까지 N-1개
+        int[] nums = new int[N-1];
+        boolean[] checks = new boolean[nums.length];
+        Arrays.fill(checks,true); //기본값이 false기 때문에 true로 변경
+        for (int i = 0; i<nums.length; i++) {
+            nums[i] = i+2; //nums에 채우기
         }
-        for (int i = 2; i*i <= N; i++) {
-            for(int j=i+1;j<=N;j++){
-                if(pn[j]%i==0)
-                    pn[j]=-1;
-            }
+
+        //2의배수 지우기
+        //checks[4]부터 지우면됨
+        int multipleOf = 2;
+        for(int i=2;i<nums.length;i+=2){
+            checks[i] = false;
         }
-        for (int i = 2; i <= N ; i++) {
-            if(pn[i]!=-1)
-                System.out.println(pn[i]);
+        for(int i=0;i<nums.length;i++){
+            if(checks[i])
+                System.out.printf("%d ",nums[i]);
         }
+
     }
 }
