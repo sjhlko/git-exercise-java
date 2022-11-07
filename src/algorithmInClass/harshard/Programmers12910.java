84b5cbf9eeec318ccc5131b6399b097c0d6d1158
서정희 <sjhlko@naver.com>
2022-11-07 10:28:46 +0900

    리스트로 푸는 방법 추가

--- src/algorithmInClass/harshard/Programmers12910.java
+++ src/algorithmInClass/harshard/Programmers12910.java
@@ -1,5 +1,8 @@
 package algorithmInClass.harshard;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class Programmers12910 {
@@ -19,6 +22,21 @@ public class Programmers12910 {
             for (int i = 0; i < ans.length; i++) {
                 ans[i] = pq.poll();
             }
+            return ans;
+        }
+        public int[] solution2(int[] arr, int divisor) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i]%divisor==0)
+                    list.add(arr[i]);
+            }
+            if(list.isEmpty())
+                list.add(-1);
+            int[] ans = new int[list.size()];
+            for (int i = 0; i < ans.length; i++) {
+                ans[i] = list.get(i);
+            }
+            Arrays.sort(ans);
             return ans;
         }
     }
