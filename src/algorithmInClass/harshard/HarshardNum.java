739bbe3086821aad414853b73a4769c077e980bc
서정희 <sjhlko@naver.com>
2022-11-07 09:50:06 +0900

    하샤드 수 - 주석 추가

--- src/algorithmInClass/harshard/HarshardNum.java
+++ src/algorithmInClass/harshard/HarshardNum.java
@@ -1,6 +1,8 @@
 package algorithmInClass.harshard;
 
 public class HarshardNum {
+    //하샤드 수
+    //https://school.programmers.co.kr/learn/courses/30/lessons/12947
     private int N;
 
     public HarshardNum(int n) {
