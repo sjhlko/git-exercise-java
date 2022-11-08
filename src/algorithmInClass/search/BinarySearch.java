240d61a3f4a3dac2e8bd7160040a1436231bdbe5
서정희 <sjhlko@naver.com>
2022-11-08 10:32:49 +0900

    이분탐색 코드업 2083번 풀이 완료

--- src/algorithmInClass/search/BinarySearch.java
+++ src/algorithmInClass/search/BinarySearch.java
@@ -3,13 +3,21 @@ package algorithmInClass.search;
 import java.util.Scanner;
 
 public class BinarySearch {
+    //https://codeup.kr/problem.php?id=2083&rid=0
+    //코드업 이분탐색
     public static void main(String[] args) {
 //        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 //        int targetNum =7;
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int targetNum = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
         int start = 0;
         int end = arr.length-1;
-        while(start!=end){
+        while(start<=end){
             int mid = (start+end)/2;
             if(arr[mid]<targetNum){
                 start = mid+1;
