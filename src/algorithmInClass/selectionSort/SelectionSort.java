b3cd54bb1290f6f604a8a0219f5cf8aea0842a47
서정희 <sjhlko@naver.com>
2022-11-09 10:29:42 +0900

    정렬기준 바꾸기 강사님 풀이 추가(템플릿 콜백 패턴, 람다식)

--- src/algorithmInClass/selectionSort/SelectionSort.java
+++ src/algorithmInClass/selectionSort/SelectionSort.java
@@ -20,4 +20,37 @@ public class SelectionSort {
         }
         System.out.println(Arrays.toString(arr));
     }
+
+    /* 강사님 풀이
+interface StatementStrategy {
+   boolean apply(int a, int b);
+}
+
+
+// interface를 선언하고
+// Template Callback패턴
+
+public class SelectionSort {
+   public int[] selectionSort(int[] arr, StatementStrategy stmt) {
+       for (int i = 0; i < arr.length - 1; i++) {
+           int minIdx = i;
+           for (int j = i; j < arr.length; j++) {
+               if (stmt.apply(arr[minIdx], arr[j])) minIdx = j;
+           }
+           int temp = arr[i];
+           arr[i] = arr[minIdx];
+           arr[minIdx] = temp;
+           System.out.println(Arrays.toString(arr));
+       }
+       return arr;
+   }
+
+   public static void main(String[] args) {
+       int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+       SelectionSort ss = new SelectionSort();
+       ss.selectionSort(arr, (a, b) -> a < b);
+       ss.selectionSort(arr, (a, b) -> a > b);
+   }
+}
+    * */
 }
