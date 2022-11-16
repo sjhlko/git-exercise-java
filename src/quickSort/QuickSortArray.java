f9172436f7c39dcb5858cb86f8d85e6b97da4a7b
서정희 <sjhlko@naver.com>
2022-11-16 09:38:38 +0900

    퀵소트 로직 구현- leftPointer rightPointer를 이용하는 방법

---
+++ src/quickSort/QuickSortArray.java
@@ -0,0 +1,30 @@
+package quickSort;
+
+import java.util.Arrays;
+
+public class QuickSortArray {
+    public void sort(int[] arr){
+        int pivot = arr[arr.length/2];
+        int leftPointer = 0;
+        int rightPointer = arr.length-1;
+        while(leftPointer<=rightPointer){
+            while(arr[leftPointer]<pivot&&leftPointer<=rightPointer)
+                leftPointer++;
+            while (arr[rightPointer]>pivot&&leftPointer<=rightPointer)
+                rightPointer--;
+            if(leftPointer<=rightPointer){
+                int tmp = arr[leftPointer];
+                arr[leftPointer]=arr[rightPointer];
+                arr[rightPointer]=tmp;
+                leftPointer++;
+                rightPointer--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+        QuickSortArray quickSortArray = new QuickSortArray();
+        quickSortArray.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
