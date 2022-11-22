0565d90b413a533506d804f2602b383bc323bc5b
서정희 <sjhlko@naver.com>
2022-11-22 10:04:36 +0900

    리스트의 값 더하기(재귀)

--- src/algorithmInClass/recursive/Print100Times.java
+++ src/algorithmInClass/recursive/Print100Times.java
@@ -1,5 +1,8 @@
 package algorithmInClass.recursive;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Print100Times {
     public void print(int n){
         if(n>100)
@@ -7,8 +10,20 @@ public class Print100Times {
         System.out.println(n);
         print(n+1);
     }
+    public int plusList(List<Integer> list){
+        if(list.isEmpty())
+            return 0;
+        return list.remove(0)+plusList(list);
+    }
+
     public static void main(String[] args) {
         Print100Times p = new Print100Times();
         p.print(1);
+        List<Integer> list = new ArrayList<>();
+        list.add(7);
+        list.add(3);
+        list.add(2);
+        list.add(9);
+        System.out.println(p.plusList(list));
     }
 }
