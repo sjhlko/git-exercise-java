be373bbb8647d10ee468d42dcfc89ab13f484294
서정희 <sjhlko@naver.com>
2022-11-23 10:49:01 +0900

    피보나치(배열)추가 및 메소드명 수정

--- src/algorithmInClass/recursive/CodeUp1855.java
+++ src/algorithmInClass/recursive/CodeUp1855.java
@@ -1,16 +1,30 @@
 package algorithmInClass.recursive;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CodeUp1855 {
-    public int Fibonacci(int n){
+    public int fibonacci(int n){
         if (n==1||n==2)
             return 1;
-        return Fibonacci(n-2)+Fibonacci(n-1);
+        return fibonacci(n-2)+fibonacci(n-1);
+    }
+
+    public int fibonacciArr(int n){
+        int[] fib = new int[n+1];
+        fib[1] = 1;
+        fib[2] = 1;
+        for(int i=3;i<=n;i++){
+            fib[i]=fib[i-1]+fib[i-2];
+        }
+        return fib[n];
     }
     public static void main(String[] args) {
         CodeUp1855 c = new CodeUp1855();
         Scanner sc = new Scanner(System.in);
-        System.out.println(c.Fibonacci(sc.nextInt()));
+        System.out.println(c.fibonacci(sc.nextInt()));
+        System.out.println(c.fibonacciArr(sc.nextInt()));
+
     }
 }
