819665f185aadc7a982cb6fe313fabd31d4559ae
서정희 <sjhlko@naver.com>
2022-11-28 09:17:26 +0900

    알파벳 출력(2개 조합)

--- src/algorithmInClass/AlphabetCombination/AlphaberCombination.java
+++ src/algorithmInClass/AlphabetCombination/AlphaberCombination.java
@@ -3,7 +3,11 @@ package algorithmInClass.AlphabetCombination;
 public class AlphaberCombination {
     public static void main(String[] args) {
         for(int i=0;i<26;i++){
-            System.out.println((char)('A'+i));
+            for(int j=0;j<26;j++){
+                System.out.print((char)('A'+i));
+                System.out.println((char)('A'+j));
+
+            }
         }
     }
 }
