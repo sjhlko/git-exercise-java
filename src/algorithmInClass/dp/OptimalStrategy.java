56492c254c6756fd8404dbb807141a977f2f54b7
sjhlko <sjhlko@naver.com>
2022-12-13 10:19:18 +0900

    OptimalStrategy_2개의 숫자만 있는 경우

--- src/algorithmInClass/dp/OptimalStrategy.java
+++ src/algorithmInClass/dp/OptimalStrategy.java
@@ -8,6 +8,14 @@ class Pair{
         this.left = left;
         this.right = right;
     }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
 }
 public class OptimalStrategy {
     public static void main(String[] args) {
@@ -15,6 +23,16 @@ public class OptimalStrategy {
         int[] arr = {2,7,40,19};
         for (int i = 0; i < 4; i++) {
             matrix[i][i] = new Pair(arr[i],0);
+            if(i>0){
+                matrix[i][i-1] = new Pair(Math.max(arr[i],arr[i-1]),Math.min(arr[i],arr[i-1]));
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            System.out.println(matrix[i][i].toString());
+        }
+        for(int i = 1; i<arr.length;i++){
+            System.out.println(matrix[i][i-1].toString());
         }
     }
+
 }
